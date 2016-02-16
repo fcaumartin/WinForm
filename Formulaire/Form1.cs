66cf6fdb962b4ca2a06efc07710ae524d3520645
frc <fcaumartin@gmail.com>
2016-02-16 15:38:00 +0100

    Mise enplace Exercice validation

--- Formulaire/Form1.cs
+++ Formulaire/Form1.cs
@@ -20,44 +20,43 @@ namespace Formulaire
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "")
+            if (textBox1.BackColor == Color.LightGreen && textBox2.BackColor == Color.LightGreen && textBox3.BackColor == Color.LightGreen && textBox4.BackColor == Color.LightGreen)
             {
-                MessageBox.Show(textBox1.Text + "\n" + textBox2.Text);
+                MessageBox.Show(textBox1.Text + "\n" + textBox2.Text + "\n" + textBox3.Text + "\n" + textBox4.Text);
+            }
+            else
+            {
+                MessageBox.Show("Pas bon");
             }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            Regex re_nom = new Regex(@"^[a-z]{2,5}$");
+            Regex re_nom = new Regex(@"^[a-zA-Z -]{1,30}$");
             
 
             if (re_nom.IsMatch(textBox1.Text))
             {
                 textBox1.BackColor = Color.LightGreen;
-                label1.Visible = false;
             }
             else
             {
-                textBox1.BackColor = Color.Red;
-                label1.Visible = true; 
+                if (textBox1.Text == "")
+                {
+                    textBox1.BackColor = SystemColors.Window;
+                }
+                else
+                {
+                    textBox1.BackColor = Color.Red;
+                }
+                
             }
         }
 
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-            //if (textBox1.Text != "")
-            //{
-            //    textBox1.BackColor = Color.LightGreen;
-            //}
-            //else
-            //{
-            //    textBox1.BackColor = Color.Red;
-            //}
-        }
-
+        
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            Regex re_date = new Regex(@"[0-3]?[0-9]/[0-1]?[0-9]/([0-9][0-9])?[0-9][0-9]");
+            Regex re_date = new Regex(@"^[0-3]?[0-9]/[0-1]?[0-9]/([0-9][0-9])?[0-9][0-9]$");
             if (re_date.IsMatch(textBox2.Text))
             {
                 textBox2.BackColor = Color.LightGreen;
@@ -66,6 +65,61 @@ namespace Formulaire
             {
                 textBox2.BackColor = Color.Red;
             }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Regex re_date = new Regex(@"^[0-9]+(,[0-9]+)?$");
+            if (re_date.IsMatch(textBox3.Text))
+            {
+                textBox3.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+            Regex re_date = new Regex(@"^[0-9]{5}$");
+            if (re_date.IsMatch(textBox4.Text))
+            {
+                textBox4.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resultat = MessageBox.Show("Etes vous sûr ?", "Attention", MessageBoxButtons.YesNo);
+
+            if (resultat == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            
+
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            
         }
     }
 }
